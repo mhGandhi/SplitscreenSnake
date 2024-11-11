509ab20e3f8bdea42cdfb70d0347d408e600b6cf
Adrian <adrian.akipi@web.de>
2024-11-11 14:51:31 +0100

    seperate tracking for total score in hope of better performance

--- src/GameResults.java
+++ src/GameResults.java
@@ -5,20 +5,29 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 public class GameResults {
     List<Map<Color,Integer>> lengthOverTime;
+    Map<Color, Integer> totalScores;
 
     public GameResults(){
         lengthOverTime = new LinkedList<Map<Color,Integer>>();
+        totalScores = new HashMap<Color,Integer>();
     }
 
     public void step(List<Player> pPlayers){
         Map<Color,Integer> tmp = new HashMap<Color,Integer>();
         for (Player p : pPlayers){
             tmp.put(p.playerConfig.color, p.occupies.size());
+            totalScores.putIfAbsent(p.playerConfig.color, 0);
+            totalScores.put(p.playerConfig.color, p.occupies.size()+totalScores.get(p.playerConfig.color));
         }
         lengthOverTime.add(tmp);
+
     }
 
-    public int getTotalScore(Color pColor) {
+    public int getTotalScore(Color pCol){
+        return totalScores.get(pCol);
+    }
+
+    public int computeTotalScore(Color pColor) {
         int tot = 0;
         for (Map<Color,Integer> m : lengthOverTime){
             tot += m.get(pColor);
